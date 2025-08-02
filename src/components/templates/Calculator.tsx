00b90bccc23331aa426b88ede0cd58df352bce5c
GuiTorres3000 <guilhermetorres3000@gmail.com>
2025-08-02 11:13:17 -0300

    feat: custom hook for calculation

--- src/components/templates/Calculator.tsx
+++ src/components/templates/Calculator.tsx
@@ -1,62 +1,13 @@
-import { useState } from "react";
+import { useCalculatorOperations } from "../../hooks/useCalculatorOperations";
 import { Card } from "../molecules/Card";
 import { CalculatorDisplay } from "../organisms/CalculatorDisplay";
 import { CalculatorKeyboard } from "../organisms/CalculatorKeyboard";
-import { useCalculatorContext } from "../../hooks/useCalculatorContext";
 
 export function Calculator() {
-
-      const [operation, setOperation] = useState('');
-      const [result, setResult] = useState('');
-      const { updateHistory } = useCalculatorContext();
-
-      const calculate = (expression: string): string => {
-            try {
-                  const sanitized = expression.replace(/[^-()\d/*+.]/g, '');
-                  const result = Function(`return (${sanitized})`)();
-                  const fixed = result.toFixed(2);
-                  const cleaned = fixed.replace(/\.?0+$/, '');
-                  return cleaned.replace('.', ',');
-            } catch {
-                  return "Erro";
-            }
-      };
-
+      const { operation, result, handleCalculation } = useCalculatorOperations();
+ 
       const handleInputClick = (name: string) => {
-            const isOperator = (char: string) => ["+", "-", "*", "/"].includes(char);
-
-            if (result) {
-                  setOperation(isNaN(Number(name)) ? `${result}${name}` : name);
-                  setResult("");
-                  return;
-            }
-            if (name === "=") {
-                  const parsedResult = calculate(operation.replace(/,/g, '.'));
-                  if (parsedResult === "Erro" || parsedResult === "Infinity") {
-                        setResult("Erro");
-                  } else {
-                        setResult(parsedResult.replace(/\./g, ","));
-                        updateHistory(operation, parsedResult);
-                  }
-                  return;
-            }
-            else if (name === "," && !operation.endsWith(",")) {
-                  setOperation(`${operation},`);
-                  return;
-            } else if (name === "C") {
-                  setOperation("");
-                  setResult("");
-                  return;
-            } else if (name === "CE") {
-                  setResult("");
-                  setOperation(operation.slice(0, -1))
-                  return;
-            } else if (isOperator(name) && isOperator(operation.slice(-1))) {
-                  setOperation(operation.slice(0, -1) + name);
-                  return;
-            } else {
-                  setOperation(`${operation}${name}`);
-            }
+            handleCalculation(name);
       };
 
       return (
